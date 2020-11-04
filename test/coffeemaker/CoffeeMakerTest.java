4397ea10b3f91a5874f6ccd4bf44939711089261
higormoles <higor_moles@usp.br>
2020-11-04 18:02:19 -0300

    Ajuste de testes

--- test/coffeemaker/CoffeeMakerTest.java
+++ test/coffeemaker/CoffeeMakerTest.java
@@ -65,20 +65,56 @@ public class CoffeeMakerTest {
     public void testAddTwoRecipesSameName() throws AmountOfRecipeException, DuplicatedRecipeException, InvalidValueException {
         boolean ok = CM.addRecipe(r1);
         assertTrue(ok);
-        r2.setName(r1.getName());
-        assertThrows(DuplicatedRecipeException.class, () -> CM.addRecipe(r2));
+        Recipe testRecipe = new Recipe("Coffee",30,7,3,1,0);
+        assertThrows(DuplicatedRecipeException.class, () -> CM.addRecipe(testRecipe));
     }
 
     @Test
     public void testAddTwoRecipesWithSameAmmountOfIngredients() throws AmountOfRecipeException, DuplicatedRecipeException, InvalidValueException {
         boolean ok = CM.addRecipe(r1);
         assertTrue(ok);
-        r1.setName("Milk");
-        assertThrows(DuplicatedRecipeException.class, () -> CM.addRecipe(r1));
+        Recipe testRecipe = new Recipe("Milk",30,4,0,1,0);
+        assertThrows(DuplicatedRecipeException.class, () -> CM.addRecipe(testRecipe));
     }
 
     @Test
-    public void testDeleteRecipe() throws RecipeException, AmountOfRecipeException, DuplicatedRecipeException {
+    public void testAddTwoRecipesWithAmtCoffeeDifferentOthersSame() throws AmountOfRecipeException, DuplicatedRecipeException, InvalidValueException {
+        boolean ok = CM.addRecipe(r1);
+        assertTrue(ok);
+        Recipe testRecipe = new Recipe("Milk",30,5,0,1,0);
+        ok = CM.addRecipe(testRecipe);
+        assertTrue(ok);
+    }
+
+    @Test
+    public void testAddTwoRecipesWithAmtMilkDifferentOthersSame() throws AmountOfRecipeException, DuplicatedRecipeException, InvalidValueException {
+        boolean ok = CM.addRecipe(r1);
+        assertTrue(ok);
+        Recipe testRecipe = new Recipe("Milk",30,4,1,1,0);
+        ok = CM.addRecipe(testRecipe);
+        assertTrue(ok);
+    }
+
+    @Test
+    public void testAddTwoRecipesWithAmtSugarDifferentOthersSame() throws AmountOfRecipeException, DuplicatedRecipeException, InvalidValueException {
+        boolean ok = CM.addRecipe(r1);
+        assertTrue(ok);
+        Recipe testRecipe = new Recipe("Milk",30,4,0,2,0);
+        ok = CM.addRecipe(testRecipe);
+        assertTrue(ok);
+    }
+
+    @Test
+    public void testAddTwoRecipesWithAmtChocolateDifferentOthersSame() throws AmountOfRecipeException, DuplicatedRecipeException, InvalidValueException {
+        boolean ok = CM.addRecipe(r1);
+        assertTrue(ok);
+        Recipe testRecipe = new Recipe("Milk",30,4,0,1,1);
+        ok = CM.addRecipe(testRecipe);
+        assertTrue(ok);
+    }
+
+    @Test
+    public void testDeleteRecipeWithOneRecipeRegistered() throws RecipeException, AmountOfRecipeException, DuplicatedRecipeException {
         boolean ok = CM.addRecipe(r1);
         assertTrue(ok);
         ok = CM.deleteRecipe("Coffee");
@@ -96,8 +132,29 @@ public class CoffeeMakerTest {
     }
 
     @Test
-    public void testDeleteRecipeNotRegistered() throws RecipeException {
-        assertThrows(RecipeException.class, () -> CM.deleteRecipe("Coffee"));
+    public void testDeleteRecipeNotRegistered() throws RecipeException, DuplicatedRecipeException, AmountOfRecipeException {
+        boolean ok = CM.addRecipe(r1);
+        assertTrue(ok);
+        assertThrows(RecipeException.class, () -> CM.deleteRecipe("Milk"));
+    }
+
+    @Test
+    public void testDeleteRecipeWithZeroRecipesRegistered() throws RecipeException, DuplicatedRecipeException, AmountOfRecipeException {
+        assertThrows(RecipeException.class, () -> CM.deleteRecipe("Milk"));
+    }
+
+    @Test
+    public void testGetRecipeRegistered() throws DuplicatedRecipeException, AmountOfRecipeException {
+        boolean ok = CM.addRecipe(r1);
+        assertTrue(ok);
+    }
+
+    @Test
+    public void testInventoryStartsWith20Amount() throws RecipeException, DuplicatedRecipeException, AmountOfRecipeException {
+        assertEquals(CM.checkCoffeeInventory(), 20);
+        assertEquals(CM.checkChocolateInventory(), 20);
+        assertEquals(CM.checkSugarInventory(), 20);
+        assertEquals(CM.checkMilkInventory(), 20);
     }
 
     @Test
@@ -107,18 +164,19 @@ public class CoffeeMakerTest {
     }
 
     @Test
-    public void testAddWrongAmmountOfCoffeeToInventory() throws InvalidValueException {
+    public void testAddWrongAmmountOfCoffeeLimitExcededByOne() throws InvalidValueException {
         assertThrows(InvalidValueException.class, () -> CM.addCoffeeInventory(81));
-    }
-
-    @Test
-    public void testSetWrongAmmountOfCoffeeToInventory() throws InvalidValueException {
-        assertThrows(InvalidValueException.class, () -> CM.addCoffeeInventory(101));
     }
 
     @Test
     public void testAddNegativeAmmountOfCoffeeToInventory() throws InvalidValueException {
         assertThrows(InvalidValueException.class, () -> CM.addCoffeeInventory(-1));
+    }
+
+    @Test
+    public void testAddZeroAmmountOfCoffeeToInventory() throws InvalidValueException {
+        CM.addCoffeeInventory(0);
+        assertEquals(CM.checkCoffeeInventory(), 20);
     }
 
     @Test
@@ -128,18 +186,19 @@ public class CoffeeMakerTest {
     }
 
     @Test
-    public void testAddWrongAmmountOfMilkToInventory() throws InvalidValueException {
+    public void testAddWrongAmmountOfMilkLimitExcededByOne() throws InvalidValueException {
         assertThrows(InvalidValueException.class, () -> CM.addMilkInventory(81));
-    }
-
-    @Test
-    public void testSetWrongAmmountOfMilkToInventory() throws InvalidValueException {
-        assertThrows(InvalidValueException.class, () -> CM.addMilkInventory(101));
     }
 
     @Test
     public void testAddNegativeAmmountOfMilkToInventory() throws InvalidValueException {
         assertThrows(InvalidValueException.class, () -> CM.addMilkInventory(-1));
+    }
+
+    @Test
+    public void testAddZeroAmmountOfMilkToInventory() throws InvalidValueException {
+        CM.addMilkInventory(0);
+        assertEquals(CM.checkMilkInventory(), 20);
     }
 
     @Test
@@ -149,18 +208,19 @@ public class CoffeeMakerTest {
     }
 
     @Test
-    public void testAddWrongAmmountOfSugarToInventory() throws InvalidValueException {
+    public void testAddWrongAmmountOfSugarLimitExcededByOne() throws InvalidValueException {
         assertThrows(InvalidValueException.class, () -> CM.addSugarInventory(81));
-    }
-
-    @Test
-    public void testSetWrongAmmountOfSugarToInventory() throws InvalidValueException {
-        assertThrows(InvalidValueException.class, () -> CM.addSugarInventory(101));
     }
 
     @Test
     public void testAddNegativeAmmountOfSugarToInventory() throws InvalidValueException {
         assertThrows(InvalidValueException.class, () -> CM.addSugarInventory(-1));
+    }
+
+    @Test
+    public void testAddZeroAmmountOfSugarToInventory() throws InvalidValueException {
+        CM.addSugarInventory(0);
+        assertEquals(CM.checkSugarInventory(), 20);
     }
 
     @Test
@@ -170,13 +230,8 @@ public class CoffeeMakerTest {
     }
 
     @Test
-    public void testAddWrongAmmountOfChocolateToInventory() throws InvalidValueException {
+    public void testAddWrongAmmountOfChocolateLimitExcededByOne() throws InvalidValueException {
         assertThrows(InvalidValueException.class, () -> CM.addChocolateInventory(81));
-    }
-
-    @Test
-    public void testSetWrongAmmountOfChocolateToInventory() throws InvalidValueException {
-        assertThrows(InvalidValueException.class, () -> CM.addChocolateInventory(101));
     }
 
     @Test
@@ -185,7 +240,13 @@ public class CoffeeMakerTest {
     }
 
     @Test
-    public void testMakeCoffee() throws InvalidValueException,InsufficientAmountOfMoneyException, RecipeException, InventoryException, AmountOfRecipeException, DuplicatedRecipeException {
+    public void testAddZeroAmmountOfChocolateToInventory() throws InvalidValueException {
+        CM.addSugarInventory(0);
+        assertEquals(CM.checkSugarInventory(), 20);
+    }
+
+    @Test
+    public void testMakeCoffeeWithoutChange() throws InvalidValueException,InsufficientAmountOfMoneyException, RecipeException, InventoryException, AmountOfRecipeException, DuplicatedRecipeException {
         boolean ok = CM.addRecipe(r1);
         assertTrue(ok);
         int change = CM.makeCoffee("Coffee", 50);
@@ -193,8 +254,18 @@ public class CoffeeMakerTest {
     }
 
     @Test
+    public void testMakeCoffeeWithChange() throws InvalidValueException,InsufficientAmountOfMoneyException, RecipeException, InventoryException, AmountOfRecipeException, DuplicatedRecipeException {
+        boolean ok = CM.addRecipe(r1);
+        assertTrue(ok);
+        int change = CM.makeCoffee("Coffee", 51);
+        assertEquals(change, 1);
+    }
+
+    @Test
     public void testMakeCoffeeWithARecipeThatDoesNotExists() throws InvalidValueException,InsufficientAmountOfMoneyException, RecipeException, InventoryException, AmountOfRecipeException, DuplicatedRecipeException {
-        assertThrows(RecipeException.class, () -> CM.makeCoffee("Coffee", 50));
+        boolean ok = CM.addRecipe(r1);
+        assertTrue(ok);
+        assertThrows(RecipeException.class, () -> CM.makeCoffee("Milk", 50));
     }
 
     @Test
@@ -205,9 +276,10 @@ public class CoffeeMakerTest {
     }
 
     @Test
-    public void testMakeCoffeeWithInsuficientCoffee() throws InvalidValueException,InsufficientAmountOfMoneyException, RecipeException, InventoryException, AmountOfRecipeException, DuplicatedRecipeException {
+    public void testMakeCoffeeWithInsufficientCoffeeDifferenceByOne() throws InvalidValueException,InsufficientAmountOfMoneyException, RecipeException, InventoryException, AmountOfRecipeException, DuplicatedRecipeException {
         boolean ok = CM.addRecipe(r1);
         assertTrue(ok);
+        CM.addCoffeeInventory(3);
         int change = CM.makeCoffee("Coffee", 50);
         assertEquals(change, 0);
         change = CM.makeCoffee("Coffee", 50);
@@ -222,28 +294,82 @@ public class CoffeeMakerTest {
     }
 
     @Test
-    public void testMakeCoffeeWithInsuficientMilk() throws InvalidValueException,InsufficientAmountOfMoneyException, RecipeException, InventoryException, AmountOfRecipeException, DuplicatedRecipeException {
+    public void testMakeCoffeeWithExactSufficientCoffee() throws InvalidValueException,InsufficientAmountOfMoneyException, RecipeException, InventoryException, AmountOfRecipeException, DuplicatedRecipeException {
+        boolean ok = CM.addRecipe(r1);
+        assertTrue(ok);
+        CM.addCoffeeInventory(4);
+        int change = CM.makeCoffee("Coffee", 50);
+        assertEquals(change, 0);
+        change = CM.makeCoffee("Coffee", 50);
+        assertEquals(change, 0);
+        change = CM.makeCoffee("Coffee", 50);
+        assertEquals(change, 0);
+        change = CM.makeCoffee("Coffee", 50);
+        assertEquals(change, 0);
+        change = CM.makeCoffee("Coffee", 50);
+        assertEquals(change, 0);
+        change = CM.makeCoffee("Coffee", 50);
+        assertEquals(change, 0);
+        int coffeeAmount = CM.checkCoffeeInventory();
+        assertEquals(coffeeAmount, 0);
+    }
+
+    @Test
+    public void testMakeCoffeeWithInsufficientMilkDifferenceByOne() throws InvalidValueException,InsufficientAmountOfMoneyException, RecipeException, InventoryException, AmountOfRecipeException, DuplicatedRecipeException {
         boolean ok = CM.addRecipe(r5);
         assertTrue(ok);
         assertThrows(InventoryException.class, () -> CM.makeCoffee("Just Milk", 100));
     }
 
     @Test
-    public void testMakeCoffeeWithInsuficientSugar() throws InvalidValueException,InsufficientAmountOfMoneyException, RecipeException, InventoryException, AmountOfRecipeException, DuplicatedRecipeException {
+    public void testMakeCoffeeWithExactSufficientMilk() throws InvalidValueException,InsufficientAmountOfMoneyException, RecipeException, InventoryException, AmountOfRecipeException, DuplicatedRecipeException {
+        boolean ok = CM.addRecipe(r5);
+        assertTrue(ok);
+        CM.addMilkInventory(1);
+        int change = CM.makeCoffee("Just Milk", 100);
+        assertEquals(change, 0);
+        int milkAmount = CM.checkMilkInventory();
+        assertEquals(milkAmount, 0);
+    }
+
+    @Test
+    public void testMakeCoffeeWithInsufficientSugarDifferenceByOne() throws InvalidValueException,InsufficientAmountOfMoneyException, RecipeException, InventoryException, AmountOfRecipeException, DuplicatedRecipeException {
         boolean ok = CM.addRecipe(r6);
         assertTrue(ok);
         assertThrows(InventoryException.class, () -> CM.makeCoffee("Just Sugar", 100));
     }
 
     @Test
-    public void testMakeCoffeeWithInsufficientChocolate() throws InvalidValueException,InsufficientAmountOfMoneyException, RecipeException, InventoryException, AmountOfRecipeException, DuplicatedRecipeException {
+    public void testMakeCoffeeWithExactSufficientSugar() throws InvalidValueException,InsufficientAmountOfMoneyException, RecipeException, InventoryException, AmountOfRecipeException, DuplicatedRecipeException {
+        boolean ok = CM.addRecipe(r6);
+        assertTrue(ok);
+        CM.addSugarInventory(1);
+        int change = CM.makeCoffee("Just Sugar", 100);
+        assertEquals(change, 0);
+        int sugarAmount = CM.checkSugarInventory();
+        assertEquals(sugarAmount, 0);
+    }
+
+    @Test
+    public void testMakeCoffeeWithInsufficientChocolateDifferenceByOne() throws InvalidValueException,InsufficientAmountOfMoneyException, RecipeException, InventoryException, AmountOfRecipeException, DuplicatedRecipeException {
         boolean ok = CM.addRecipe(r7);
         assertTrue(ok);
         assertThrows(InventoryException.class, () -> CM.makeCoffee("Just Chocolate", 100));
     }
 
     @Test
-    public void testGetRecipes() throws AmountOfRecipeException, DuplicatedRecipeException {
+    public void testMakeCoffeeWithExactSufficientChocolate() throws InvalidValueException,InsufficientAmountOfMoneyException, RecipeException, InventoryException, AmountOfRecipeException, DuplicatedRecipeException {
+        boolean ok = CM.addRecipe(r7);
+        assertTrue(ok);
+        CM.addChocolateInventory(1);
+        int change = CM.makeCoffee("Just Chocolate", 100);
+        assertEquals(change, 0);
+        int chocolateAmount = CM.checkChocolateInventory();
+        assertEquals(chocolateAmount, 0);
+    }
+
+    @Test
+    public void testGetRecipesWithOneRecipeRegistered() throws AmountOfRecipeException, DuplicatedRecipeException {
         boolean ok = CM.addRecipe(r1);
         assertTrue(ok);
         recipeArray = CM.getRecipes();
@@ -251,9 +377,37 @@ public class CoffeeMakerTest {
     }
 
     @Test
+    public void testGetRecipesWithMoreThanOneRecipeRegistered() throws AmountOfRecipeException, DuplicatedRecipeException {
+        boolean ok = CM.addRecipe(r1);
+        assertTrue(ok);
+        ok = CM.addRecipe(r2);
+        assertTrue(ok);
+        recipeArray = CM.getRecipes();
+        assertTrue(recipeArray.contains(r1));
+        assertTrue(recipeArray.contains(r2));
+    }
+
+    @Test
+    public void testSetValidAmountOfChocolateInInventory() throws InvalidValueException {
+        IN.setChocolate(0);
+        assertEquals(IN.getChocolate(), 0);
+        IN.setChocolate(100);
+        assertEquals(IN.getChocolate(), 100);
+    }
+
+
+    @Test
     public void testSetInvalidAmountOfChocolateInInventory() throws InvalidValueException {
         assertThrows(InvalidValueException.class, () -> IN.setChocolate(-1));
         assertThrows(InvalidValueException.class, () -> IN.setChocolate(101));
+    }
+
+    @Test
+    public void testSetValidAmountOfCoffeeInInventory() throws InvalidValueException {
+        IN.setCoffee(0);
+        assertEquals(IN.getCoffee(), 0);
+        IN.setCoffee(100);
+        assertEquals(IN.getCoffee(), 100);
     }
 
     @Test
@@ -263,14 +417,48 @@ public class CoffeeMakerTest {
     }
 
     @Test
+    public void testSetValidAmountOfMilkInInventory() throws InvalidValueException {
+        IN.setMilk(0);
+        assertEquals(IN.getMilk(), 0);
+        IN.setMilk(100);
+        assertEquals(IN.getMilk(), 100);
+    }
+
+    @Test
     public void testSetInvalidAmountOfMilkInInventory() throws InvalidValueException {
         assertThrows(InvalidValueException.class, () -> IN.setMilk(-1));
         assertThrows(InvalidValueException.class, () -> IN.setMilk(101));
     }
 
     @Test
+    public void testSetValidAmountOfSugarInInventory() throws InvalidValueException {
+        IN.setSugar(0);
+        assertEquals(IN.getSugar(), 0);
+        IN.setSugar(100);
+        assertEquals(IN.getSugar(), 100);
+    }
+
+    @Test
     public void testSetInvalidAmountOfSugarInInventory() throws InvalidValueException {
         assertThrows(InvalidValueException.class, () -> IN.setSugar(-1));
         assertThrows(InvalidValueException.class, () -> IN.setSugar(101));
+    }
+
+    @Test
+    public void testGetAmountOfCoffeeInInventory() {
+        assertEquals(IN.getCoffee(), 20);
+    }
+
+    @Test
+    public void testGetAmountOfMilkInInventory() { assertEquals(IN.getMilk(), 20); }
+
+    @Test
+    public void testGetAmountOfSugarInInventory() {
+        assertEquals(IN.getSugar(), 20);
+    }
+
+    @Test
+    public void testGetAmountOfChocolateInInventory() {
+        assertEquals(IN.getChocolate(), 20);
     }
 }
--- test/coffeemaker/RecipeTest.java
+++ test/coffeemaker/RecipeTest.java
@@ -9,22 +9,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RecipeTest {
-
-    private Recipe r3;
-
-    @BeforeEach
-    public  void setUp() throws Exception {
-        r3 = new Recipe("Latte",75,3,1,1,0);
-    }
-
     @Test
-    public void testCreateValidRecipe() throws InvalidValueException {
-        Recipe recipe = new Recipe("Coffee",50,4,0,1,0);
-    }
-
-    @Test
-    public void testCreateInvalidRecipeAmtCoffee() throws InvalidValueException {
-        assertThrows(InvalidValueException.class, () ->new Recipe("Coffee",50,-1,0,1,0));
+    public void testOneIngredientAmountBiggerThanZero() throws InvalidValueException {
+        Recipe recipe = new Recipe("Coffee",50,4,0,0,0);
     }
 
     @Test
@@ -33,13 +20,23 @@ public class RecipeTest {
     }
 
     @Test
+    public void testPriceBiggerThanZero() throws InvalidValueException {
+       Recipe recipe = new Recipe("Coffee",30,1,2,0,0);
+    }
+
+    @Test
+    public void testPriceEqualsOne() throws InvalidValueException {
+        Recipe recipe = new Recipe("Coffee",1,1,2,0,0);
+    }
+
+    @Test
     public void testCreateRecipeWithPriceEqualsToZero() throws InvalidValueException {
         assertThrows(InvalidValueException.class, () ->new Recipe("Coffee",0,1,1,1,1));
     }
 
     @Test
-    public void testCreateRecipeWithNegativePrice() throws InvalidValueException {
-        assertThrows(InvalidValueException.class, () ->new Recipe("Coffee",-1,1,1,1,1));
+    public void testPriceEqualsMinusOne() throws InvalidValueException {
+        assertThrows(InvalidValueException.class, () ->new Recipe("Coffee",-1,1,2,0,0));
     }
 
     @Test
@@ -53,8 +50,43 @@ public class RecipeTest {
     }
 
     @Test
+    public void testCreateRecipeWithChocolateAmountEqualsZero() throws InvalidValueException {
+        Recipe recipe = new Recipe("Coffee",50,1,1,1,0);
+    }
+
+    @Test
+    public void testCreateRecipeWithChocolateAmountEqualsOne() throws InvalidValueException {
+        Recipe recipe = new Recipe("Coffee",50,0,0,0,1);
+    }
+
+    @Test
     public void testCreateRecipeWithNegativeChocolateAmount() throws InvalidValueException {
         assertThrows(InvalidValueException.class, () ->new Recipe("Coffee",50,1,1,1,-1));
+    }
+
+    @Test
+    public void testCreateRecipeWithCoffeeAmountEqualsZero() throws InvalidValueException {
+        Recipe recipe = new Recipe("Coffee",50,0,1,1,1);
+    }
+
+    @Test
+    public void testCreateRecipeWithCoffeeAmountEqualsOne() throws InvalidValueException {
+        Recipe recipe = new Recipe("Coffee",50,1,0,0,0);
+    }
+
+    @Test
+    public void testCreateRecipeWithNegativeCoffeeAmount() throws InvalidValueException {
+        assertThrows(InvalidValueException.class, () -> new Recipe("Coffee", 50, -1, 1, 1, 1));
+    }
+
+    @Test
+    public void testCreateRecipeWithMilkAmountEqualsZero() throws InvalidValueException {
+        Recipe recipe = new Recipe("Coffee",50,1,0,1,1);
+    }
+
+    @Test
+    public void testCreateRecipeWithMilkAmountEqualsOne() throws InvalidValueException {
+        Recipe recipe = new Recipe("Coffee",50,0,1,0,0);
     }
 
     @Test
@@ -63,12 +95,17 @@ public class RecipeTest {
     }
 
     @Test
-    public void testCreateRecipeWithNegativeSugarAmount() throws InvalidValueException {
-        assertThrows(InvalidValueException.class, () ->new Recipe("Coffee",50,1,1,-1,1));
+    public void testCreateRecipeWithSugarAmountEqualsZero() throws InvalidValueException {
+        Recipe recipe = new Recipe("Coffee", 50, 1, 1, 0, 1);
     }
 
     @Test
-    public void testCreateRecipeWithNegativeCoffeeAmount() throws InvalidValueException {
-        assertThrows(InvalidValueException.class, () -> new Recipe("Coffee", 50, -1, 1, 1, 1));
+    public void testCreateRecipeWithSugarAmountEqualsOne() throws InvalidValueException {
+        Recipe recipe = new Recipe("Coffee", 50, 0, 0, 1, 0);
+    }
+
+    @Test
+    public void testCreateRecipeWithNegativeSugarAmount() throws InvalidValueException {
+        assertThrows(InvalidValueException.class, () -> new Recipe("Coffee", 50, 1, 1, -1, 1));
     }
 }
